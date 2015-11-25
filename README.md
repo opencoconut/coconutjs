c5e00d9a452abdb53f1c12f82ca3c4b0ae9b8638
Bruno Celeste <bruno.celeste@gmail.com>
2015-11-25 15:57:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# NodeJS client Library for encoding Videos with Coconut
+
+## Install
+
+```console
+npm install coconutjs
+```
+
+## Submitting the job
+
+Use the [API Request Builder](https://app.coconut.co/job/new) to generate a config file that match your specific workflow.
+
+Example of `coconut.conf`:
+
+```ini
+var s3 = s3://accesskey:secretkey@mybucket
+
+set webhook = http://mysite.com/webhook/coconut?videoId=$vid
+
+-> mp4  = $s3/videos/video_$vid.mp4
+-> webm = $s3/videos/video_$vid.webm
+-> jpg_300x = $s3/previews/thumbs_#num#.jpg, number=3
+```
+
+Here is the javascript code to submit the config file:
+
+```javascript
+var coconut = require('coconut');
+
+coconut.createJob({
+  'api_key': 'k-api-key',
+  'conf': 'coconut.conf',
+  'source': 'https://s3-eu-west-1.amazonaws.com/files.coconut.co/test.mp4',
+  'vars': {'vid': 1234}
+}, function(job) {
+  if(job.status == 'ok') {
+    console.log(job.id);
+  } else {
+    console.log(job.error_code);
+    console.log(job.error_message);
+  }
+});
+```
+
+You can also create a job without a config file. To do that you will need to give every settings in the method parameters. Here is the exact same job but without a config file:
+
+```javascript
+vid = 1234
+s3 = 's3://accesskey:secretkey@mybucket'
+
+coconut.createJob({
+  'api_key': 'k-api-key',
+  'source': 'http://yoursite.com/media/video.mp4',
+  'webhook': 'http://mysite.com/webhook/coconut?videoId=' + vid,
+  'outputs': {
+    'mp4': s3 + '/videos/video_' + vid + '.mp4',
+    'webm': s3 + '/videos/video_' + vid + '.webm',
+    'jpg_300x': s3 + '/previews/thumbs_#num#.jpg, number=3'
+  }
+}, function(job) {
+  //...
+});
+```
+
+Note that you can use the environment variable `COCONUT_API_KEY` to set your API key.
+
+*Released under the [MIT license](http://www.opensource.org/licenses/mit-license.php).*
+
+---
+
+* Coconut website: http://coconut.co
+* API documentation: http://coconut.co/docs
+* Github: http://github.com/opencoconut/coconut.rb
+* Contact: [support@coconut.co](mailto:support@coconut.co)
+* Twitter: [@OpenCoconut](http://twitter.com/opencoconut)
---
+++ test.js
@@ -0,0 +1,117 @@
+var coconut = require('./coconut');
+var fs = require('fs');
+
+exports.testSubmitConfig = function(test) {
+  conf = coconut.config({
+    'source': 'https://s3-eu-west-1.amazonaws.com/files.coconut.co/test.mp4',
+    'webhook': 'http://mysite.com/webhook',
+    'outputs': {'mp4': 's3://a:s@bucket/video.mp4'}
+  });
+
+  coconut.submit(conf, null, function(job) {
+    test.notEqual(undefined, job);
+    test.equal('ok', job.status);
+    test.ok(job.id > 0);
+    test.done();
+  });
+};
+
+exports.testSubmitBadConfig = function(test) {
+  conf = coconut.config({
+    'source': 'https://s3-eu-west-1.amazonaws.com/files.coconut.co/test.mp4'
+  });
+
+  coconut.submit(conf, null, function(job) {
+    test.notEqual(undefined, job);
+    test.equal('error', job.status);
+    test.equal('config_not_valid', job.error_code);
+    test.done();
+  });
+};
+
+exports.testSubmitConfigWithAPIKey = function(test) {
+  conf = coconut.config({
+    'source': 'https://s3-eu-west-1.amazonaws.com/files.coconut.co/test.mp4'
+  });
+
+  coconut.submit(conf, 'k-4d204a7fd1fc67fc00e87d3c326d9b75', function(job) {
+    test.notEqual(undefined, job);
+    test.equal('error', job.status);
+    test.equal('authentication_failed', job.error_code);
+    test.done();
+  });
+};
+
+exports.testGenerateFullConfigWithNoFile = function(test) {
+  conf = coconut.config({
+    'vars': {
+      'vid': 1234,
+      'user': 5098,
+      's3': 's3://a:s@bucket'
+    },
+    'source': 'https://s3-eu-west-1.amazonaws.com/files.coconut.co/test.mp4',
+    'webhook': 'http://mysite.com/webhook?vid=$vid&user=$user',
+    'outputs': {
+      'mp4': '$s3/vid.mp4',
+      'webm': '$s3/vid.webm',
+      'jpg_200x': '$s3/thumb.jpg'
+    }
+  });
+
+  generated = [
+    'var s3 = s3://a:s@bucket',
+    'var user = 5098',
+    'var vid = 1234',
+    '',
+    'set source = https://s3-eu-west-1.amazonaws.com/files.coconut.co/test.mp4',
+    'set webhook = http://mysite.com/webhook?vid=$vid&user=$user',
+    '',
+    '-> jpg_200x = $s3/thumb.jpg',
+    '-> mp4 = $s3/vid.mp4',
+    '-> webm = $s3/vid.webm'
+  ].join("\n")
+
+  test.equal(generated, conf);
+  test.done();
+}
+
+exports.testGenerateConfigWithFile = function(test) {
+  fs.writeFileSync('coconut.conf', "var s3 = s3://a:s@bucket/video\nset webhook = http://mysite.com/webhook?vid=$vid&user=$user\n-> mp4 = $s3/$vid.mp4");
+
+  conf = coconut.config({
+    'conf': 'coconut.conf',
+    'source': 'https://s3-eu-west-1.amazonaws.com/files.coconut.co/test.mp4',
+    'vars': {'vid': 1234, 'user': 5098}
+  });
+
+  generated = [
+    'var s3 = s3://a:s@bucket/video',
+    'var user = 5098',
+    'var vid = 1234',
+    '',
+    'set source = https://s3-eu-west-1.amazonaws.com/files.coconut.co/test.mp4',
+    'set webhook = http://mysite.com/webhook?vid=$vid&user=$user',
+    '',
+    '-> mp4 = $s3/$vid.mp4'
+  ].join("\n")
+
+  test.equal(generated, conf);
+  fs.unlink('coconut.conf');
+  test.done();
+}
+
+exports.testSubmitFile = function(test) {
+  fs.writeFileSync('coconut.conf', "set webhook = http://mysite.com/webhook?vid=$vid&user=$user\n-> mp4 = s3://a:s@bucket/video/$vid.mp4");
+
+  coconut.createJob({
+    'conf': 'coconut.conf',
+    'source': 'https://s3-eu-west-1.amazonaws.com/files.coconut.co/test.mp4',
+    'vars': {'vid': 1234, 'user': 5098}
+  }, function(job) {
+    test.equal('ok', job.status);
+    test.ok(job.id > 0);
+    fs.unlink('coconut.conf');
+    test.done();
+  });
+
+}
